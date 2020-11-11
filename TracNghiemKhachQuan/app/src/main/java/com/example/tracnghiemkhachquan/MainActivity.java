817ace66f94300124d584b9dd18d5ded5f6f95e9
nguyen tien <57231517+baobaote00@users.noreply.github.com>
2020-11-11 11:12:04 +0700

    :heart:

---
+++ TracNghiemKhachQuan/app/src/main/java/com/example/tracnghiemkhachquan/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tracnghiemkhachquan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cau_hoi_2);
+    }
+}
